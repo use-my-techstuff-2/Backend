a740fc4e1de0e65a5a33b1ebbd01b9c0da5eacd9
Robert Stepanov <robertdstepanov@gmail.com>
2020-03-02 12:48:53 -0500

    Bugs

--- gadgets/router.js
+++ gadgets/router.js
@@ -1,28 +1,39 @@
-const router = require('express').Router();
-const Owners = require('../owners/owners-model');
-const Gadgets = require('./gadgets-model');
+const router = require("express").Router();
+const Owners = require("../owners/owners-model");
+const Gadgets = require("./gadgets-model");
 
-const authentication  = require('../auth/auth-middleware');
+const authentication = require("../auth/auth-middleware");
 
-router.get('/:ownerId', authentication, (req, res) => {
-    if(req.owner.id.toString() === req.params.ownerId) {
-        Gadgets.findByOwnerrId(req.params.ownerId)
-        .then((gadgets) => {
-            if (gadgets.length === 0) {
-                return res.status(400).json({ error: 'no gadgets to display' });
-            } else {
-                return res.status(200).json(gadgets);
-            }
-        })
-        .catch((error) => {
-            		console.log(error);
-            		res.status(500).json({ error: 'error retrieving gadgets' });
-            	});
-    } else {
-        return res.status(400).json({ message: 'Are you logged in to the correct account?' })
-    }
+router.get("/", (req, res) => {
+  Gadgets.find()
+    .then(gadgets => res.status(200).json(gadgets))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "Could not retrieve the gadgets from the database" })
+    );
 });
 
+router.get("/:ownerId", authentication, (req, res) => {
+  if (req.owner.id.toString() === req.params.ownerId) {
+    Gadgets.findByOwnerrId(req.params.ownerId)
+      .then(gadgets => {
+        if (gadgets.length === 0) {
+          return res.status(400).json({ error: "no gadgets to display" });
+        } else {
+          return res.status(200).json(gadgets);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: "error retrieving gadgets" });
+      });
+  } else {
+    return res
+      .status(400)
+      .json({ message: "Are you logged in to the correct account?" });
+  }
+});
 
 /** 
  * @api {get} /api/gadgets/:ownerId GET owners gadgets
@@ -55,44 +66,42 @@ router.get('/:ownerId', authentication, (req, res) => {
 ]
  **/
 
-router.get('/gadget/:gadgetId', (req, res) => {
-        Gadgets.findById(req.params.gadgetId)
-        .then((gadget) => {
-            if (!gadget) {
-                return res.status(400).json({ error: 'no gadget to display' });
-            } else {
-                return res.status(200).json(gadget);
-            }
+router.get("/gadget/:gadgetId", (req, res) => {
+  Gadgets.findById(req.params.gadgetId)
+    .then(gadget => {
+      if (!gadget) {
+        return res.status(400).json({ error: "no gadget to display" });
+      } else {
+        return res.status(200).json(gadget);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "error retrieving gadgets" });
+    });
+});
+
+router.post("/:ownerId", (req, res) => {
+  const gadget = { ...req.body, owner_id: req.params.ownerId };
+  Owners.findById(req.params.ownerrId).then(owner => {
+    if (!owner) {
+      res.status(401).json({ error: "owner doesn't exist" });
+    } else {
+      Workouts.add(gadget)
+        .then(gadget => {
+          if (!gadget) {
+            res.status(400).json({ error: "missing fields" });
+          } else {
+            res.status(201).json(gadget);
+          }
         })
-        .catch((error) => {
-            		console.log(error);
-            		res.status(500).json({ error: 'error retrieving gadgets' });
+        .catch(error => {
+          console.log(error);
+          res.status(500).json({ error: "error posting new gadget" });
         });
-})
-
-
-router.post('/:ownerId', (req, res) => {
-    const gadget = {...req.body, owner_id: req.params.ownerId};
-    Users.findById(req.params.ownerrId)
-        .then((owner) => {
-            if (!owner) {
-				res.status(401).json({ error: "owner doesn't exist" });
-            } else {
-                Workouts.add(gadget)
-                    .then(gadget => {
-                        if (!gadget) {
-                            res.status(400).json({error: 'missing fields'})
-                        } else {
-                            res.status(201).json(gadget)
-                        }
-                    })
-                    .catch(error => {
-                        console.log(error)
-                        res.status(500).json({error: "error posting new gadget"})
-                    })
-            }
-        })
-})
+    }
+  });
+});
 
 /** 
  * @api {post} /api/gadgets/:ownerId POST new gadget
@@ -119,20 +128,20 @@ router.post('/:ownerId', (req, res) => {
 ]
  **/
 
-router.put('/:ownerId', (req, res) => {
-    Gadgets.update(req.body.id, req.body)
+router.put("/:ownerId", (req, res) => {
+  Gadgets.update(req.body.id, req.body)
     .then(updateGadget => {
-        if (!updateGadget) {
-            res.status(400).json({error: "missing field"})
-        } else {
-            res.status(200).json(updateGadget)
-        }
+      if (!updateGadget) {
+        res.status(400).json({ error: "missing field" });
+      } else {
+        res.status(200).json(updateGadget);
+      }
     })
     .catch(error => {
-        console.log(error)
-        res.status(500).json({error: "error updating gadget"})
-    })
-})
+      console.log(error);
+      res.status(500).json({ error: "error updating gadget" });
+    });
+});
 
 /** 
  * @api {put} /api/gadgets/:ownerId PUT update gadget
@@ -161,16 +170,16 @@ router.put('/:ownerId', (req, res) => {
 ]
  **/
 
-router.delete('/:gadgetId', (req, res) => {
-    Gadgets.remove(req.params.gadgetId)
-        .then(() => {
-            res.status(200).json({ message: 'Gadget deleted. Good job.' })
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({ message: 'Error deleting gadget' })
-        })
-})
+router.delete("/:gadgetId", (req, res) => {
+  Gadgets.remove(req.params.gadgetId)
+    .then(() => {
+      res.status(200).json({ message: "Gadget deleted. Good job." });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Error deleting gadget" });
+    });
+});
 
 /** 
  * @api {delete} /api/gadgets/:gadgetId DELETE gadget
